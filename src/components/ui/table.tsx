91da0fc08d20cdce3996f7668a6bf0ce55e47760
Md Fuad Hasan <web.hasanmdfuad@gmail.com>
2025-05-16 15:27:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <tr>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <RedirectErrorBoundary router={{...}}>
    >       <InnerLayoutRouter url="/customers" tree={[...]} cacheNode={{lazyData:null, ...}} segmentPath={[...]}>
    >         <ClientPageRoot Component={function CustomersPage} searchParams={{}} params={{}}>
    >           <CustomersPage params={Promise} searchParams={Promise}>
    >             <div className="flex flex-...">
    >               <PageHeader>
    >               <div>
    >               <div className="flex-grow ...">
    >                 <div className="overflow-y...">
    >                   <_c>
    >                     <div className="relative w...">
    >                       <table ref={null} className="w-full cap...">
    >                         <_c2 className="sticky top...">
    >                           <thead ref={null} className={"[&_tr]:b..."}>
    >                             <_c8>
    > >                             <tr
    > >                               ref={null}
    > >                               className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted"
    > >                             >
    >                                 <_c10>
    >                                 <_c10>
    >                                 <_c10>
    >                                 <_c10>
    >                                 <_c10>
    >                                 <_c10>
    > >                               {" "}
    >                         ...
    >               ...
    >         ...
    >
    >
    > src/components/ui/table.tsx (77:10) @ _c8
    >
    >   75 |   React.HTMLAttributes<HTMLTableRowElement>
    >   76 | >(({ className, children, ...props }, ref) => {
    > > 77 |   return <tr ref={ref} className={cn("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)} {...props}>{children}</tr>;
    >      |          ^
    >   78 | });
    >   79 | TableRow.displayName = "TableRow";
    >   80 |
    >
    > Call Stack
    > 17
    >
    > Show 14 ignore-listed frame(s)
    > tr
    > <anonymous> (0:0)
    > _c8
    > src/components/ui/table.tsx (77:10)
    > CustomersPage
    > src/app/(app)/customers/page.tsx (256:17)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,16 +1,12 @@
 
 import * as React from "react";
-
-// Assuming this is a utility function like clsx or similar for merging class names
 import { cn } from "@/lib/utils";
 
-// The main Table component, wrapped in a div for responsive overflow
 const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, children, ...props }, ref) => (
   <div className="relative w-full overflow-auto">
-    {/* The <table> element should contain all table content (caption, thead, tbody, tfoot) */}
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
@@ -22,40 +18,26 @@ const Table = React.forwardRef<
 ));
 Table.displayName = "Table";
 
-// Table Header component
 const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, children, ...props }, ref) => (
-  // Thead element for table header group
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props}>
-    {children}
-  </thead>
+  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props}>{children}</thead>
 ));
 TableHeader.displayName = "TableHeader";
 
-// Table Body component
 const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, children, ...props }, ref) => (
-  // Tbody element for table body group
-  <tbody
-    ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
-    {...props}
-  >
-    {children}
-  </tbody>
+  <tbody ref={ref} className={cn("[&_tr:last-child]:border-0", className)} {...props}>{children}</tbody>
 ));
 TableBody.displayName = "TableBody";
 
-// Table Footer component
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, children, ...props }, ref) => (
-  // Tfoot element for table footer group
   <tfoot
     ref={ref}
     className={cn(
@@ -63,13 +45,10 @@ const TableFooter = React.forwardRef<
       className
     )}
     {...props}
-  >
-    {children}
-  </tfoot>
+  >{children}</tfoot>
 ));
 TableFooter.displayName = "TableFooter";
 
-// Table Row component
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
@@ -78,12 +57,10 @@ const TableRow = React.forwardRef<
 });
 TableRow.displayName = "TableRow";
 
-// Table Head Cell component
 const TableHead = React.forwardRef<
   HTMLTableCellElement,
   React.ThHTMLAttributes<HTMLTableCellElement>
 >(({ className, children, ...props }, ref) => (
-  // Th element for table header cells
   <th
     ref={ref}
     className={cn(
@@ -91,45 +68,34 @@ const TableHead = React.forwardRef<
       className
     )}
     {...props}
-  >
-    {children}
-  </th>
+  >{children}</th>
 ));
 TableHead.displayName = "TableHead";
 
-// Table Cell component
 const TableCell = React.forwardRef<
   HTMLTableCellElement,
   React.TdHTMLAttributes<HTMLTableCellElement>
 >(({ className, children, ...props }, ref) => (
-  // Td element for table data cells
   <td
     ref={ref}
     className={cn("px-2 py-2 align-middle text-xs sm:px-4 [&:has([role=checkbox])]:pr-0", className)}
     {...props}
-  >
-    {children}
-  </td>
+  >{children}</td>
 ));
 TableCell.displayName = "TableCell";
 
-// Table Caption component
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
   React.HTMLAttributes<HTMLTableCaptionElement>
 >(({ className, children, ...props }, ref) => (
-  // Caption element for table caption
   <caption
     ref={ref}
     className={cn("mt-4 text-sm text-muted-foreground", className)}
     {...props}
-  >
-    {children}
-  </caption>
+  >{children}</caption>
 ));
 TableCaption.displayName = "TableCaption";
 
-// Export the components for use
 export {
   Table,
   TableHeader,
