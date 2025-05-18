3e8e59f6cb6b74fc7ba1251a38fc21a09597d941
Md Fuad Hasan <web.hasanmdfuad@gmail.com>
2025-05-18 07:39:42 +0000

    borders of the tables are not rounded . fix that for all tables

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,7 +6,18 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, children, ...props }, ref) => {
-  return <div className="relative w-full overflow-auto"><table ref={ref} className={cn("w-full caption-bottom text-sm", className)} {...props}>{children}</table></div>;
+  return (
+    // Added rounded-lg to this wrapper div
+    <div className="relative w-full overflow-auto rounded-lg"> 
+      <table
+        ref={ref}
+        className={cn("w-full caption-bottom text-sm", className)}
+        {...props}
+      >
+        {children}
+      </table>
+    </div>
+  );
 });
 Table.displayName = "Table";
 
